fd4b2a6ffd1c883e172bb19b509fda314a60c396
unknown <ig_999@yahoo.com>
2019-07-30 15:38:24 +0300

    Added tag,vr,vm

--- DICOM-Viewer/DICOM-Viewer/DICOMViewer.cpp
+++ DICOM-Viewer/DICOM-Viewer/DICOMViewer.cpp
@@ -4,12 +4,6 @@ DICOMViewer::DICOMViewer(QWidget *parent) : QMainWindow(parent)
 {
 	ui.setupUi(this);
 	ui.tableWidget->horizontalHeader()->setStretchLastSection(true);
-	DcmFileFormat test;
-}
-
-void DICOMViewer::fileHovered(QAction * qaction)
-{
-
 }
 
 void DICOMViewer::closeButtonClicked()
@@ -23,9 +17,53 @@ void DICOMViewer::fileTriggered(QAction* qaction)
 	if (option == "Open")
 	{
 		QString fileName = QFileDialog::getOpenFileName(this);
+		if (file.loadFile(fileName.toStdString().c_str()).good())
+		{
+			extractData(file);
+		}
+		else
+		{
+			QMessageBox* messageBox = new QMessageBox();
+			messageBox->setText("Failed to open DICOM file!\n");
+			messageBox->exec();
+		}
 	}
 	else
 	{
-
+		
 	}
+}
+
+void DICOMViewer::extractData(DcmFileFormat file)
+{
+	DcmMetaInfo* metaInfo = file.getMetaInfo();
+	DcmDataset* dataSet = file.getDataset();
+
+	for (int i = 0; i < dataSet->card(); i++)
+	{
+		insertInTable(dataSet->getElement(i),i);
+	}
+	for (int i = 0; i < metaInfo->card(); i++)
+	{
+		insertInTable(metaInfo->getElement(i), i);
+	}
+}
+
+void DICOMViewer::insertInTable(DcmElement* element, int index)
+{
+	DcmTagKey tagKey = DcmTagKey(OFstatic_cast(Uint16, element->getGTag()), OFstatic_cast(Uint16, element->getETag()));
+	QTableWidgetItem* itemTag = new QTableWidgetItem();
+	QTableWidgetItem* itemVR = new QTableWidgetItem();
+	QTableWidgetItem* itemVM = new QTableWidgetItem();
+	QTableWidgetItem* itemLength = new QTableWidgetItem();
+	itemTag->setText(QString::fromStdString(tagKey.toString().c_str()));
+	DcmVR* vr = new DcmVR(element->getVR());
+	itemVR->setText(QString::fromStdString(vr->getVRName()));
+	itemVM->setText(QString::fromStdString(std::to_string(element->getVM())));
+	itemLength->setText(QString::fromStdString(std::to_string(element->getLength())));
+	ui.tableWidget->insertRow(index);
+	ui.tableWidget->setItem(index, 0, itemTag);
+	ui.tableWidget->setItem(index, 1, itemVR);
+	ui.tableWidget->setItem(index, 2, itemVM);
+	ui.tableWidget->setItem(index, 3, itemLength);
 }
--- DICOM-Viewer/DICOM-Viewer/DICOMViewer.h
+++ DICOM-Viewer/DICOM-Viewer/DICOMViewer.h
@@ -2,8 +2,11 @@
 
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/qfiledialog.h>
+#include <QtWidgets/qmessagebox.h>
 #include "ui_DICOMViewer.h"
 #include "dcmtk/dcmdata/dcfilefo.h"
+#include "dcmtk/dcmdata/dcmetinf.h"
+#include "dcmtk/dcmdata/dcvr.h"
 
 class DICOMViewer : public QMainWindow
 {
@@ -14,9 +17,13 @@ class DICOMViewer : public QMainWindow
 
 	private:
 		Ui::DICOMViewerClass ui;
+		DcmFileFormat file;
+
+	protected:
+		void insertInTable(DcmElement* element, int index);
+		void extractData(DcmFileFormat file);
 	
 	private slots:
 		void fileTriggered(QAction* qaction);
-		void fileHovered(QAction* qaction);
 		void closeButtonClicked();
 };
